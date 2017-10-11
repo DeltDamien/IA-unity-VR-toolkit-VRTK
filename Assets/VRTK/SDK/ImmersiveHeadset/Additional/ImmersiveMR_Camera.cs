3d52d643e30863b72021808c8baf0fbdcd7a55e0
tomwim <samo.miw@gmail.com>
2017-10-11 10:03:18 +0200

    Add ImmersiveMR_Camera to track the camera and TrackedObject for controllers. Also add ImmersiveMRHeadset

---
+++ Assets/VRTK/SDK/ImmersiveHeadset/Additional/ImmersiveMR_Camera.cs
@@ -0,0 +1,68 @@
+﻿namespace VRTK
+{
+    using UnityEngine;
+    using UnityEngine.XR;
+
+    /// <summary>
+    /// Camera script for the main camera for Immersive Mixed Reality. 
+    /// </summary>
+    [RequireComponent (typeof(Camera))]
+    public class ImmersiveMR_Camera : MonoBehaviour
+    {
+        /// <summary>
+        /// Name of the Windows Mixed Reality Device as listed in XRSettings.
+        /// </summary>
+        private const string DEVICE_NAME = "WindowsMR";
+        
+        void Start()
+        {
+            if (CheckForMixedRealitySupport())
+            {
+                SetupMRCamera();
+            }
+        }
+
+        /// <summary>
+        /// Check if the Mixed (Virtual) Reality Settings are properly set.
+        /// </summary>
+        /// <returns>Are the settings set.</returns>
+        private bool CheckForMixedRealitySupport()
+        {
+            if(XRSettings.enabled == false)
+            {
+                Debug.LogError("XRSettings are not enabled. Enable in PlayerSettings. Do not forget to add Windows Mixed Reality to Virtual Reality SDKs.");
+                return false;
+            }
+            else
+            {
+                foreach(string device in XRSettings.supportedDevices)
+                {
+                    if(device.Equals("WindowsMR"))
+                    {
+                        Debug.LogError("Windows Mixed Reality is not supported in XRSettings, add in PlayerSettings.");
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Setup the MR camera properly.
+        /// </summary>
+        private void SetupMRCamera()
+        {
+            Camera camera = GetComponent<Camera>();
+            if (camera.tag != "MainCamera")
+            {
+                camera.tag = "MainCamera";
+            }
+
+            if(camera.stereoTargetEye != StereoTargetEyeMask.Both)
+            {
+                Debug.LogError("Target eye of main camera is not set to both. Are you sure you want to render only one eye?");
+            }
+        }
+    }
+}
---
+++ Assets/VRTK/SDK/ImmersiveHeadset/Additional/ImmersiveMR_TrackedObject.cs
@@ -0,0 +1,287 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+namespace VRTK
+{
+    public class ImmersiveMR_TrackedObject : MonoBehaviour
+    {
+
+        private struct ButtonState
+        {
+            //
+            // Summary:
+            //     ///
+            //     Normalized amount ([0, 1]) representing how much select is pressed.
+            //     ///
+            public float SelectPressedAmount { get; set; }
+            //
+            // Summary:
+            //     ///
+            //     Depending on the InteractionSourceType of the interaction source, this returning
+            //     true could represent a number of equivalent things: main button on a blicker,
+            //     air-tap on a hand, and the trigger on a motion controller.
+            //     ///
+            public bool SelectPressed { get; set; }
+            //
+            // Summary:
+            //     ///
+            //     Whether or not the menu button is pressed.
+            //     ///
+            public bool MenuPressed { get; set; }
+            //
+            // Summary:
+            //     ///
+            //     Whether the controller is grasped.
+            //     ///
+            public bool Grasped { get; set; }
+            //
+            // Summary:
+            //     ///
+            //     Whether or not the touchpad is touched.
+            //     ///
+            public bool TouchpadTouched { get; set; }
+            //
+            // Summary:
+            //     ///
+            //     Whether or not the touchpad is pressed, as if a button.
+            //     ///
+            public bool TouchpadPressed { get; set; }
+            //
+            // Summary:
+            //     ///
+            //     Normalized coordinates for the position of a touchpad interaction.
+            //     ///
+            public Vector2 TouchpadPosition { get; set; }
+            //
+            // Summary:
+            //     ///
+            //     Normalized coordinates for the position of a thumbstick.
+            //     ///
+            public Vector2 ThumbstickPosition { get; set; }
+            //
+            // Summary:
+            //     ///
+            //     Whether or not the thumbstick is pressed.
+            //     ///
+            public bool ThumbstickPressed { get; set; }
+        }
+
+        [SerializeField]
+        private InteractionSourceHandedness handedness;
+
+        private uint index;
+        public uint Index { get { return index; } }
+
+        private ButtonState currentButtonState;
+
+        private Vector3 angularVelocity;
+        public Vector3 AngularVelocity { get { return angularVelocity; } }
+
+        public float hairTriggerDelta = 0.1f; // amount trigger must be pulled or released to change state
+        private float hairTriggerLimit;
+        private bool hairTriggerState;
+        private bool hairTriggerPrevState;
+
+        private void Start()
+        {
+            InteractionManager.InteractionSourceDetected += InteractionManager_InteractionSourceDetected;
+            InteractionManager.InteractionSourceUpdated += InteractionManager_InteractionSourceUpdated;
+            InteractionManager.InteractionSourceLost += InteractionManager_InteractionSourceLost;
+
+            // TODO: Check if needed
+            InteractionManager.InteractionSourcePressed += InteractionManager_InteractionSourcePressed;
+        }
+
+    #region Getter functions
+        public bool GetPress(InteractionSourcePressType button)
+        {
+            return true;
+        }
+
+        public bool GetPressDown(InteractionSourcePressType button)
+        {
+            return true;
+        }
+
+        public bool GetPressUp(InteractionSourcePressType button)
+        {
+            return true;
+        }
+
+        public bool GetTouch(InteractionSourcePressType button)
+        {
+            return true;
+        }
+
+        public bool GetTouchDown(InteractionSourcePressType button)
+        {
+            return true;
+        }
+
+        public bool GetTouchUp(InteractionSourcePressType button)
+        {
+            return true;
+        }
+
+        public Vector2 GetAxis(InteractionSourcePressType button)
+        {
+            switch(button)
+            {
+                case InteractionSourcePressType.Touchpad:
+                    return currentButtonState.TouchpadPosition;
+
+                case InteractionSourcePressType.Thumbstick:
+                    return currentButtonState.ThumbstickPosition;
+            }
+
+            return Vector2.zero;
+        }
+
+        public bool GetHairTrigger()
+        {
+            // Update(); Needed?
+            return hairTriggerState;
+        }
+
+        public bool GetHairTriggerDown()
+        {
+            //Update(); Needed?
+            return hairTriggerState && !hairTriggerPrevState;
+        }
+        public bool GetHairTriggerUp()
+        {
+            //Update(); Needed?
+            return !hairTriggerState && hairTriggerPrevState;
+        }
+        #endregion
+
+        #region Event callbacks
+        private void InteractionManager_InteractionSourceDetected(InteractionSourceDetectedEventArgs args)
+        {
+            InteractionSourceState state = args.state;
+            InteractionSource source = state.source;
+
+            if (source.kind == InteractionSourceKind.Controller && source.handedness == handedness)
+            {
+                index = source.id;
+                currentButtonState = new ButtonState();
+            }
+        }
+
+        private void InteractionManager_InteractionSourceLost(InteractionSourceLostEventArgs args)
+        {
+            InteractionSourceState state = args.state;
+            InteractionSource source = state.source;
+
+            if (source.kind == InteractionSourceKind.Controller && source.handedness == handedness)
+            {
+                //TODO: delete something or make it gone or whatever helps
+            }
+        }
+
+        private void InteractionManager_InteractionSourceUpdated(InteractionSourceUpdatedEventArgs args)
+        {
+            // TODO: is this also triggered on pressed? Is it for position?
+            InteractionSourceState state = args.state;
+
+            UpdateInteractionState(state);
+        }
+
+        private void InteractionManager_InteractionSourcePressed(InteractionSourcePressedEventArgs args)
+        {
+
+        }
+        #endregion
+
+        #region Update functions
+        private void UpdateStates()
+        {
+            InteractionSourceState[] states = InteractionManager.GetCurrentReading();
+
+            foreach(InteractionSourceState state in states)
+            {
+                UpdateInteractionState(state);
+            }
+        }
+
+        private void UpdateInteractionState(InteractionSourceState state)
+        {
+            InteractionSource source = state.source;
+
+            if (source.handedness == handedness && source.id == index)
+            {
+                currentButtonState.SelectPressed = state.selectPressed;
+                currentButtonState.SelectPressedAmount = state.selectPressedAmount;
+
+                if (source.supportsGrasp)
+                {
+                    currentButtonState.Grasped = state.grasped;
+                }
+                else if (source.supportsMenu)
+                {
+                    currentButtonState.MenuPressed = state.menuPressed;
+                }
+                else if (source.supportsThumbstick)
+                {
+                    currentButtonState.ThumbstickPosition = state.thumbstickPosition;
+                    currentButtonState.ThumbstickPressed = state.thumbstickPressed;
+                }
+                else if (source.supportsTouchpad)
+                {
+                    currentButtonState.TouchpadPosition = state.touchpadPosition;
+                    currentButtonState.TouchpadPressed = state.touchpadPressed;
+                    currentButtonState.TouchpadTouched = state.touchpadTouched;
+                }
+
+                UpdateAngularVelocity(state.sourcePose);
+                UpdateControllerPose(state.sourcePose);
+            }
+        }
+
+        private void UpdateControllerPose(InteractionSourcePose pose)
+        {
+            Quaternion newRotation;
+            if (pose.TryGetRotation(out newRotation, InteractionSourceNode.Grip))
+            {
+
+            }
+
+            Vector3 newPosition;
+            if (pose.TryGetPosition(out newPosition, InteractionSourceNode.Grip))
+            {
+
+            }
+        }
+
+        private void UpdateAngularVelocity(InteractionSourcePose pose)
+        {
+            Vector3 newAngularVelocity;
+            if(pose.TryGetAngularVelocity(out newAngularVelocity))
+            {
+                angularVelocity = newAngularVelocity;
+            }
+        }
+
+        private void UpdateHairTrigger()
+        {
+            hairTriggerPrevState = hairTriggerState;
+            float value = currentButtonState.SelectPressedAmount;
+
+            if (hairTriggerState)
+            {
+                if (value < hairTriggerLimit - hairTriggerDelta || value <= 0.0f)
+                    hairTriggerState = false;
+            }
+            else
+            {
+                if (value > hairTriggerLimit + hairTriggerDelta || value >= 1.0f)
+                    hairTriggerState = true;
+            }
+
+            hairTriggerLimit = hairTriggerState ? Mathf.Max(hairTriggerLimit, value) : Mathf.Min(hairTriggerLimit, value);
+        }
+        #endregion
+    }
+}
