cb57d8df43618b953cfa656365f9f441752e17a3
tomwim <samo.miw@gmail.com>
2017-10-11 10:01:36 +0200

    Add first draft of ImmersiveMR defines and system

---
+++ Assets/VRTK/SDK/ImmersiveHeadset/SDK_ImmersiveMRDefines.cs
@@ -0,0 +1,61 @@
+﻿namespace VRTK
+{
+    using System;
+
+    /// <summary>
+    /// Handles all the scripting define symbols for the Immersive Mixed Reality SDK.
+    /// </summary>
+    public static class SDK_ImmersiveMRDefines
+    {
+        /// <summary>
+        /// The scripting define symbol for the Immersive Mixed Reality SDK.
+        /// </summary>
+        public const string ScriptingDefineSymbol = SDK_ScriptingDefineSymbolPredicateAttribute.RemovableSymbolPrefix + "SDK_IMMERSIVEMR";
+
+        private const string BuildTargetGroupName = "Standalone";
+
+        // TODO: Version checking (see below)
+
+        /*
+        [SDK_ScriptingDefineSymbolPredicate(ScriptingDefineSymbol, BuildTargetGroupName)]
+        [SDK_ScriptingDefineSymbolPredicate(SDK_ScriptingDefineSymbolPredicateAttribute.RemovableSymbolPrefix + "OCULUS_UTILITIES_1_12_0_OR_NEWER", BuildTargetGroupName)]
+        private static bool IsUtilitiesVersion1120OrNewer()
+        {
+            Version wrapperVersion = GetOculusWrapperVersion();
+            return wrapperVersion != null && wrapperVersion >= new Version(1, 12, 0);
+        }
+
+        [SDK_ScriptingDefineSymbolPredicate(ScriptingDefineSymbol, BuildTargetGroupName)]
+        [SDK_ScriptingDefineSymbolPredicate(SDK_ScriptingDefineSymbolPredicateAttribute.RemovableSymbolPrefix + "OCULUS_UTILITIES_1_11_0_OR_OLDER", BuildTargetGroupName)]
+        private static bool IsUtilitiesVersion1110OrOlder()
+        {
+            Version wrapperVersion = GetOculusWrapperVersion();
+            return wrapperVersion != null && wrapperVersion < new Version(1, 12, 0);
+        }
+
+        [SDK_ScriptingDefineSymbolPredicate(AvatarScriptingDefineSymbol, BuildTargetGroupName)]
+        private static bool IsAvatarAvailable()
+        {
+            return (IsUtilitiesVersion1120OrNewer() || IsUtilitiesVersion1110OrOlder())
+                   && VRTK_SharedMethods.GetTypeUnknownAssembly("OvrAvatar") != null;
+        }
+
+        private static Version GetOculusWrapperVersion()
+        {
+            Type pluginClass = VRTK_SharedMethods.GetTypeUnknownAssembly("OVRPlugin");
+            if (pluginClass == null)
+            {
+                return null;
+            }
+
+            FieldInfo versionField = pluginClass.GetField("wrapperVersion", BindingFlags.Public | BindingFlags.Static);
+            if (versionField == null)
+            {
+                return null;
+            }
+
+            return (Version)versionField.GetValue(null);
+        }
+        */
+    }
+}
---
+++ Assets/VRTK/SDK/ImmersiveHeadset/SDK_ImmersiveMRSystem.cs
@@ -0,0 +1,38 @@
+﻿namespace VRTK
+{
+//#if VRTK_DEFINE_SDK_STEAMVR
+    using UnityEngine.XR.WSA;
+
+    [SDK_Description("ImmersiveMR", SDK_ImmersiveMRDefines.ScriptingDefineSymbol, "WSA_XR", "Standalone")]
+    public class SDK_ImmersiveMRSystem : SDK_BaseSystem
+    {
+        /// <summary>
+        /// The ForceInterleavedReprojectionOn method determines whether Interleaved Reprojection should be forced on or off.
+        /// </summary>
+        /// <param name="force">If true then Interleaved Reprojection will be forced on, if false it will not be forced on.</param>
+        public override void ForceInterleavedReprojectionOn(bool force)
+        {
+            // TODO: Compare with Oculus and SteamVR
+        }
+
+        /// <summary>
+        /// The IsDisplayOnDesktop method returns true if the display is extending the desktop.
+        /// </summary>
+        /// <returns>Returns true if the display is extending the desktop</returns>
+        public override bool IsDisplayOnDesktop()
+        {
+            // TODO: Compare with Oculus and SteamVR
+            return false;
+        }
+
+        /// <summary>
+        /// The ShouldAppRenderWithLowResources method is used to determine if the Unity app should use low resource mode. Typically true when the dashboard is showing.
+        /// </summary>
+        /// <returns>Returns true if the Unity app should render with low resources.</returns>
+        public override bool ShouldAppRenderWithLowResources()
+        {
+            // TODO: Compare with Oculus and SteamVR
+            return false;
+        }
+    }
+}
